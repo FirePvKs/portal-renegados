02aefe77c557b27f11135015c1419b0db2042a28
FirePvKs <vicentepaolourbinariquelme@gmail.com>
2026-06-03 21:35:39 -0400

    se cambio el mapa

--- backend/src/server.js
+++ backend/src/server.js
@@ -54,5 +54,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`🥷 Backend en http://localhost:${PORT}`);
+  console.log(`Backend en http://localhost:${PORT}`);
 });
